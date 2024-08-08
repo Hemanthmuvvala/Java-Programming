32832b1b26575750140ce90be103ffa3cffedda3
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-08-08 11:54:58 +0530

    Create CLA.java

---
+++ CLA.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class CLA{
+public static void main(String[]args){
+int total=0,a[]={100,200,150,190,2000,1000};
+for(int i:a){
+
+total=total+i;
+System.out.println(total);
+}
+System.out.println(total);
+}
+
+
+
+
+
+}
