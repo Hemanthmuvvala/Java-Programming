1f92b482465b9c452b3d9771dc026deac47c35fb
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-08-22 12:19:39 +0530

    Create Constructor1.java

---
+++ Constructor1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Constructor1 {
+  
+    Constructor1() {
+        System.out.println("This is a constructor");
+    }
+
+    
+
+    public static void main(String[] args) {
+        Constructor1 obj1 = new Constructor1();
+       
+    }
+}
