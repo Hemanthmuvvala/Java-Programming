42fa7d4d4a982b19bb713e5a3405f1afe90de1b0
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-08-22 12:30:40 +0530

    Create Method.java

---
+++ Method.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Method{
+public static int Mul(int a,int b)
+{
+return a*b;
+}
+public static void main (String[]args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the value of a and b");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("The product of a and b= "+Mul(a,b));
+
+
+
+
+}
+
+
+
+
+
+
+}
