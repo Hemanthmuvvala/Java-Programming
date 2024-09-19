06438b5484da1787101c05705ca90b2b6c486dad
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-09-19 11:15:05 +0530

    Create Single_inheritance.java

---
+++ EX-4/Single_inheritance.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Inherit {
+    void display() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Inherit obj1 = new Inherit();
+        obj1.display();
+
+        Child obj2 = new Child();
+        obj2.display();
+    }
+
+    public static class Child extends Inherit {
+        void display() {
+            System.out.println("World");
+        }
+    }
+}
