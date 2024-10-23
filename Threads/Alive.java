498a2e8454445a514aa94799a3d7c9ce7d12819e
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-10-23 19:09:40 +0530

    Update Alive.java

--- Threads/Alive.java
+++ Threads/Alive.java
@@ -1,56 +1,42 @@
-class a extends Thread {
-    @Override
+class First extends Thread {
     public void run() {
-        while (true) {
-            System.out.println("Good morning");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("hi");
             try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
+                Thread.sleep(1000); // Pauses for 1 second
+            } catch (Exception e) {
                 System.out.println(e);
             }
         }
     }
 }
 
-class b extends Thread {
-    @Override
+class Second extends Thread {
     public void run() {
-        while (true) {
-            System.out.println("Hello");
+        for (int j = 0; j < 5; j++) {
+            System.out.println("hello");
             try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
+                Thread.sleep(1000); // Pauses for 1 second
+            } catch (Exception e) {
                 System.out.println(e);
             }
         }
     }
 }
 
-class c extends Thread {
-    @Override
-    public void run() {
-        while (true) {
-            System.out.println("Welcome");
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                System.out.println(e);
-            }
-        }
-    }
-}
+public class Checks  {
 
-public class Threadcla {
-    public static void main(String[] args) throws Exception {
-        a t1 = new a();
-        b t2 = new b();
-        c t3 = new c();
-       
-        t1.start();
-        t2.start();
-        t3.start();
-        t1.join();
-        t2.join();
-        t3.join();
+    public static void main(String[] args) throws  Exception {
+        First f = new First();
+        Second s = new Second();
+        f.start(); // Start First thread
+        s.start();
+        System.out.println(f.isAlive()); // Start Second thread
+       f.join();
+        s.join();
+        System.out.println("bye");
+        System.out.println(s.isAlive());
+        
+
     }
 }
