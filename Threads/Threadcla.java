605681d67b144b25622c3427b328a662d7045370
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-10-23 19:17:44 +0530

    Update Threadcla.java

--- Threads/Threadcla.java
+++ Threads/Threadcla.java
@@ -1,11 +1,8 @@
-//.Write a JAVA program that creates threads by extending Thread class.First thread display 
-//“Good Morning “every 1 sec, the second thread displays “Hello “every 2 seconds and the 
-//third display “Welcome” every 3 seconds,(Repeat the same by implementing Runnable).
 class a extends Thread {
     @Override
     public void run() {
-        System.out.println("Good morning");
         while (true) {
+            System.out.println("Good morning");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -18,38 +15,42 @@ class a extends Thread {
 class b extends Thread {
     @Override
     public void run() {
-        System.out.println("Hello");
-        while(true){
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            System.out.println(e);
+        while (true) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
         }
     }
-}
 }
 
 class c extends Thread {
     @Override
     public void run() {
-        System.out.println("Welcome");
-        while(true){
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            System.out.println(e);
+        while (true) {
+            System.out.println("Welcome");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+            }
         }
     }
 }
-}
 
 public class Threadcla {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         a t1 = new a();
         b t2 = new b();
         c t3 = new c();
+       
         t1.start();
         t2.start();
         t3.start();
+        t1.join();
+        t2.join();
+        t3.join();
     }
 }
