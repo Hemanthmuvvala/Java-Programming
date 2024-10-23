2c4be65e3a6cc0cec03fa4d64301c90c9f994e36
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-10-23 10:49:41 +0530

    Update EX-4

---
+++ EX-4/Sineritance.java
@@ -0,0 +1,26 @@
+class a {
+    
+    public void parent()
+    {
+        System.out.println("i am parent");
+        
+    }
+}
+class b extends a{
+            public void child()
+            {
+               
+                System.out.println("iam child class");
+            }
+       
+    
+    
+}
+public class Sinheritance{
+    public static void main(String[]args){
+        a ob1=new a();
+        ob1.parent();
+        b ob2=new b();
+        ob2.parent();
+    }
+}
