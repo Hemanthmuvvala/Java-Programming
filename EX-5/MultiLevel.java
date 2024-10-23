14c2029a464d5f814a47769fe212086679924bde
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-10-23 12:13:18 +0530

    Create MultiLevel.java

---
+++ EX-5/MultiLevel.java
@@ -0,0 +1,46 @@
+
+ class guest
+ {
+
+    public void read()
+    {
+    
+        System.out.println("this is guest login");
+    }
+    }
+class dev extends guest{
+
+public  void write(){
+System.out.println("this is developer login");
+
+}
+
+
+}
+class admin extends dev{
+
+public void show(){
+    System.out.println("this is admin login");
+}
+@Override
+public void read(){
+    super.read();
+    System.out.println("i am the admin to the database");
+}
+
+
+}
+public class MultiLevel {
+    public static void main(String[] args) {
+        guest g=new guest();
+        dev d=new dev();
+        admin a=new admin();
+        g.read();
+        d.read();
+        d.write();
+        a.read();
+        a.write();
+        a.show();
+       
+}
+}
