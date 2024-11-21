58a99132d213658cf80faff698afe569360c2d20
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-11-21 13:59:49 +0530

    Update Arm_strong.java

--- Arm_strong.java
+++ Arm_strong.java
@@ -1,29 +1,30 @@
-
 import java.util.*;
 
 class Arm_strong {
     @SuppressWarnings("resource")
     public static void main(String[] args) {
-        int sum = 0;
-        int a, t, rem;
-      
+        int sum = 0, a, t, rem, digits = 0;
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the value of a:");
         a = sc.nextInt();
-        t = a; 
-        
+        t = a;
+
+        int temp = a;
+        while (temp > 0) {
+            temp = temp / 10;
+            digits++;
+        }
+
         while (a > 0) {
             rem = a % 10;
-            sum = sum + (int)Math.pow(rem,3);
+            sum = sum + (int) Math.pow(rem, digits);
             a = a / 10;
         }
-        
-        if (t==sum) {
+
+        if (t == sum) {
             System.out.println("The given number is an armstrong number");
         } else {
             System.out.println("The given number is not an armstrong number");
         }
-        
-       
     }
 }
